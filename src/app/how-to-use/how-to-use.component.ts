9ad7d54e6276bc562f0c2d2abf6932296a220ce2
kasmello <karmel.gandahusada@gmail.com>
2024-06-04 21:44:48 +0800

    misc

--- src/app/how-to-use/how-to-use.component.ts
+++ src/app/how-to-use/how-to-use.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-how-to-use',
@@ -6,4 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./how-to-use.component.css']
 })
 export class HowToUseComponent {
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  itemNumber = 1;
+
+  nextItem() {
+    console.log(Math.min(3,this.itemNumber+1));
+    this.itemNumber = Math.min(3,this.itemNumber+1);
+    this.changePage()
+    
+  }
+
+  prevItem() {
+    console.log(Math.max(1,this.itemNumber-1));
+    this.itemNumber = Math.max(1,this.itemNumber-1);
+    this.changePage()
+  }
+
+  changeItem(num: number) {
+    this.itemNumber = num
+  }
+
+
+  changePage() {
+
+    const element = this.el.nativeElement.querySelector(`.button-${this.itemNumber}`);
+    this.renderer.listen(element, 'click', () => {});
+    element.click(); // Simulate the click event
+    
+  }
+
 }
